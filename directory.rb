2cdfc6f8e3510a343fd2200a05923d96ccc1a815
Megan Folsom <mfolsom@gmail.com>
2014-01-07 12:41:32 +0000

    Added array with students and ability to count

---
+++ directory.rb
@@ -0,0 +1,6 @@
+puts "The students of my cohort at Makers Academy"
+puts "___________________________________________"
+
+students = ["Bugs Bunny", "Road Runner", "Coyote", "Daffy Duck", "Donald Duck","The Abominable Snowman"]
+puts students
+puts "overall we have #{students.count} students."
