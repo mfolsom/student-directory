826532fb749d3ecf906331c31dc0b4cfcd3752a0
Megan Folsom <mfolsom@gmail.com>
2014-01-07 12:47:20 +0000

    iterate over students array and added comments

--- directory.rb
+++ directory.rb
@@ -1,6 +1,13 @@
+#list all the students in an array
+students = ["Bugs Bunny", "Road Runner", "Coyote", "Daffy Duck", "Donald Duck","The Abominable Snowman"]
+
+#print them
 puts "The students of my cohort at Makers Academy"
 puts "___________________________________________"
 
-students = ["Bugs Bunny", "Road Runner", "Coyote", "Daffy Duck", "Donald Duck","The Abominable Snowman"]
-puts students
-puts "overall we have #{students.count} students."
+students.each do |student|
+	puts student
+end
+
+#print final count
+puts "overall we have #{students.length} students."
