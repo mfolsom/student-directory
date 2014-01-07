b8cc4f503cebbf3d358376d149f2fcdeed4ddc6a
Megan Folsom <mfolsom@gmail.com>
2014-01-07 18:42:53 +0000

    updated with shebang for reals

--- directory.rb
+++ directory.rb
@@ -1,8 +1,9 @@
+#!/usr/bin/env ruby
 #list all the students in an array
 students = [
 	{:name => "Bugs Bunny", :cohort => :January},
 	{:name => "Road Runner",:cohort => :November},
-	{:name => "Coyote", :cohort => :Janury},
+	{:name => "Coyote", :cohort => :January},
 	{:name => "Daffy Duck", :cohort => :November},
 	{:name => "Donald Duck", :cohort => :January},
 	{:name => "The Abominable Snowman", :cohort => :January}
