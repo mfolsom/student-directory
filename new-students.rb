5d48014f4123c65ac4fc514b75c5d1f7e2dfd646
Megan Folsom <mfolsom@gmail.com>
2014-01-08 13:50:38 +0000

    replace puts with print

--- new-students.rb
+++ new-students.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students"
+    print "To finish, just hit return twice"
 
 	#empty array
 	students = []
@@ -11,27 +11,27 @@ def input_students
 
 	while !name.empty? do 
 		students << {:name => name, :cohort => :january}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		name = gets.chomp
 	end
 	students
 end
 
 def print_header
-puts "The students of my cohort at Makers Academy"
-puts "___________________________________________"
+print "The students of my cohort at Makers Academy\n"
+print "___________________________________________"
 end
 
 def print(students)
 students.each do |student|
-	puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	print "#{student[:name]} (#{student[:cohort]} cohort)\n"
 end
 
 end
 
 def print_footer(names)
 #print final count
-puts "overall we have #{names.length} students."
+print "overall we have #{names.length} students.\n"
 end
 
 students = input_students
