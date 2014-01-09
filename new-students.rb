203b432a1b52b023faac69fc2abf318df254672e
Megan Folsom <mfolsom@gmail.com>
2014-01-09 11:12:28 +0000

    removed extraneous comments and fixed but with student count

--- new-students.rb
+++ new-students.rb
@@ -36,11 +36,9 @@ end
 
 def user_choice
 	puts "Which cohort do you want to see?"
-	# cohorts =[]
 	choice = gets.chomp
 	choice.upcase!
-	# cohorts << choice
-	# cohorts
+
 
 end
 
@@ -58,35 +56,17 @@ end
 
 def output(students, cohorts)
 
-#	students.each {|month|} 
-#	puts "#{students[0]}"
-# students.map {|hash|}
-# hash.select {|name, cohort| [:name, :cohort].include? cohort}
-	#puts students
  	sorted_by_cohort=students.sort {|student, cohort| student[:cohort]<=>student[:name]}
  	x = sorted_by_cohort.select{ |student, cohort| student[:cohort] == cohorts}.map{|student, cohort| student[:name]}
  	puts x
 
-	#students.select{|student| student[:cohort] == "JAN" }.each_with_index do |student, i|
-	#	puts "#{i+1} : #{student[:name]} (#{student[:cohort]} cohort)".center(100)
-	#end
 end
-# @filtered_list = []
-# def filter_students(students)
 
-# 	students.each do |student|
 
-# 		if student[:name].length <= 12
-# 			@filtered_list << {student => :name, student=> :cohort}
-# 			puts "#{student[:name]}:#{student[:cohort]}".center(100)
-# 		end
-# 	end
-# end
-
-def print_footer(names)
+def print_footer(names, cohorts)
 #print final count
 	if names.count > 1
-		puts "overall we have #{names.count{|student| student[:cohort] == "JAN"}} students.".center(100)
+		puts "overall we have #{names.count{|student| student[:cohort] == cohorts}} students.".center(100)
 	elsif names.count == 1
 		puts "There is a student! yay!".center(100)
 	else
@@ -101,4 +81,4 @@ cohorts = user_choice
 print_header(students,cohorts)
 output(students,cohorts)
 #filter_students(students)
-print_footer(students)
+print_footer(students, cohorts)
