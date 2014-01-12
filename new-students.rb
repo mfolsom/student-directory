e56095fe433e66727deffcf4cedb2b3f0886edf7
Megan Folsom <mfolsom@gmail.com>
2014-01-12 20:59:51 +0000

    playing around wiht if statements to see if I can make them more efficient

--- new-students.rb
+++ new-students.rb
@@ -7,21 +7,17 @@ def input_students
 	puts "First name?".ljust(0)
 
 	students = []
-	#get the first name
 	name = gets.chomp
 	name.capitalize!
 	puts "cohort?".ljust(0)
 	month = gets.chomp.upcase!
+	
 
 	while !name.empty? && !month.empty? do 
 		students << {:name => name, :cohort => month}
-			if students.count > 1
-			puts "Now there are #{students.length} students!".center(100)
-			
-			else
-			puts "Now there is #{students.length} student".center(100)
-		
-			end
+		student_counts = "Now there are #{students.length} students!".center(100)
+        one_student = "Now there is #{students.length} student".center(100)
+		puts(students.length > 1 ? student_counts : one_student)
 		puts "First name?".ljust(0)
 		name = gets.chomp
 		name.capitalize!
@@ -35,16 +31,14 @@ def user_choice
 	puts "Which cohort do you want to see?"
 	choice = gets.chomp.upcase!
 
-
-
 end
 
 def print_header(students, cohorts)
 	
-		puts "The students of the #{cohorts} cohort at Makers Academy".center(100) if students.count > 1
-		puts "The student of the #{cohorts} cohort at Makers Academy".center(100) if students.count == 1
-		puts "There are no students (sad face)!".center(100) if students.count.zero?
-		puts "___________________________________________".center(100)
+	puts "The students of the #{cohorts} cohort at Makers Academy".center(100) if students.count > 1
+	puts "The student of the #{cohorts} cohort at Makers Academy".center(100) if students.count == 1
+	puts "There are no students (sad face)!".center(100) if students.count.zero?
+	puts "___________________________________________".center(100)
 end
 
 def output(students, cohorts)
@@ -57,10 +51,10 @@ end
 
 def print_footer(names, cohorts)
    number = names.count{|student| student[:cohort] == cohorts}
- 
-    puts "Hey why don't you enroll?".center(100) if names.count.zero? 
-    puts "there is a student! yay!".center(100) if names.count == 1
-    puts "we have #{number} students in this cohort.".center(100) if names.count > 1
+   no_names = "Hey why don't you enroll?".center(100)
+   one_name = "there is a student! yay!".center(100)
+   several_names = "we have #{number} students in this cohort.".center(100)
+   puts(names.count.zero? ? no_names : names.count ==1 ? one_name : several_names)
 
 end
 
